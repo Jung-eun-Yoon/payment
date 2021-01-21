14b056387cd4c224939737d3e523b6f322d3647c
Jung-eun-Yoon <75401910+Jung-eun-Yoon@users.noreply.github.com>
2021-01-21 09:09:59 +0900

    Update Payment.java

--- src/main/java/matching/Payment.java
+++ src/main/java/matching/Payment.java
@@ -2,6 +2,9 @@ package matching;
 
 import javax.persistence.*;
 import org.springframework.beans.BeanUtils;
+import org.springframework.transaction.support.TransactionSynchronizationAdapter;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
 import java.util.List;
 
 @Entity
@@ -12,13 +15,26 @@ public class Payment {
     private Long matchId;
     private Integer price;
     private String paymentAction;
-
+    
     @PostPersist
     public void onPostPersist(){
         PaymentApproved paymentApproved = new PaymentApproved();
         BeanUtils.copyProperties(this, paymentApproved);
-        paymentApproved.publishAfterCommit();
 
+        //바로 이벤트를 보내버리면 주문정보가 커밋되기도 전에 배송발송됨 이벤트가 발송되어 주문테이블의 상태가 바뀌지 않을 수 있다.
+        // TX 리스너는 커밋이 완료된 후에 이벤트를 발생하도록 만들어준다.
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
+            @Override
+            public void beforeCommit(boolean readOnly) {
+                paymentApproved.publish();
+            }
+        });
+        try {
+            //istio test를 위해 payment sleep 추가가
+           Thread.currentThread().sleep((long) (400 + Math.random() * 220));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -53,8 +69,5 @@ public class Payment {
     public void setPaymentAction(String paymentAction) {
         this.paymentAction = paymentAction;
     }
-
-
-
 
 }
